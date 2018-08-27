f253a5fefa8147bd7ecb063d98caa39ffd108726
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-08-27 16:06:27 +0700

    delete interface Repository, class CustomerRepositoryImpl and edit interface CustomerRepository

--- src/main/java/com/codegym/repository/CustomerRepository.java
+++ src/main/java/com/codegym/repository/CustomerRepository.java
@@ -1,7 +1,8 @@
 package com.codegym.repository;
 
 import com.codegym.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CustomerRepository extends Repository<Customer> {
+public interface CustomerRepository extends PagingAndSortingRepository <Customer, Long>{
 
 }
