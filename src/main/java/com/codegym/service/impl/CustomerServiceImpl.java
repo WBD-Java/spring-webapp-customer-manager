92de09286e5dcff594179fc3c085e6dbd9a28d5d
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-08-28 16:10:51 +0700

    add method findAllByProvince

--- src/main/java/com/codegym/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/codegym/service/impl/CustomerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.codegym.service.impl;
 
 import com.codegym.model.Customer;
+import com.codegym.model.Province;
 import com.codegym.repository.CustomerRepository;
 import com.codegym.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,11 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public Iterable<Customer> findAll() {
         return customerRepository.findAll();
+    }
+
+    @Override
+    public Iterable<Customer> findAllByProvince(Province province) {
+        return customerRepository.findAllByProvince(province);
     }
 
     @Override
