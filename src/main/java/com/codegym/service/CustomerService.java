e80689a488d37b889499f70394258ef9231426de
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-08-28 17:43:58 +0700

    update method findAll to return page

--- src/main/java/com/codegym/service/CustomerService.java
+++ src/main/java/com/codegym/service/CustomerService.java
@@ -2,9 +2,11 @@ package com.codegym.service;
 
 import com.codegym.model.Customer;
 import com.codegym.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CustomerService {
-    Iterable<Customer> findAll();
+    Page<Customer> findAll(Pageable pageable);
 
     Iterable<Customer> findAllByProvince(Province province);
 
--- src/main/java/com/codegym/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/codegym/service/impl/CustomerServiceImpl.java
@@ -5,14 +5,16 @@ import com.codegym.model.Province;
 import com.codegym.repository.CustomerRepository;
 import com.codegym.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
     @Override
-    public Iterable<Customer> findAll() {
-        return customerRepository.findAll();
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
     }
 
     @Override
