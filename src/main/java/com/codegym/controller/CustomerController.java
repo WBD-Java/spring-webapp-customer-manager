163ac325e20734a37e7ec4b86ae34517ebf85ab8
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-08-28 12:06:17 +0700

    add method to get province

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.codegym.controller;
 
 import com.codegym.model.Customer;
+import com.codegym.model.Province;
 import com.codegym.service.CustomerService;
+import com.codegym.service.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,14 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+
+    @Autowired
+    private ProvinceService provinceService;
+
+    @ModelAttribute("provinces")
+    public Iterable<Province> provinces() {
+        return provinceService.findAll();
+    }
 
     @GetMapping("/customers")
     public ModelAndView listCustomer() {
