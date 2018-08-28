713682713b52b6077448c7790c46976be9eea5d7
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-08-28 09:39:11 +0700

    update function edit Province

--- src/main/java/com/codegym/controller/ProvinceController.java
+++ src/main/java/com/codegym/controller/ProvinceController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -37,6 +38,25 @@ public class ProvinceController {
         ModelAndView modelAndView = new ModelAndView("/province/create");
         modelAndView.addObject("province", new Province());
         modelAndView.addObject("message", "New province create successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/province/edit/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Province province = provinceService.findById(id);
+
+        ModelAndView modelAndView = new ModelAndView("/province/edit");
+        modelAndView.addObject("province", province);
+        return modelAndView;
+    }
+
+    @PostMapping("/province/edit")
+    public ModelAndView updateProvince(@ModelAttribute("province") Province province) {
+        provinceService.save(province);
+
+        ModelAndView modelAndView = new ModelAndView("/province/edit");
+        modelAndView.addObject("province", province);
+        modelAndView.addObject("message", "Province update successfully!");
         return modelAndView;
     }
 }
